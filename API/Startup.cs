f72c019e644a995fb4cdd2457e0b02cca0e2bacb
boris.milostov <boris.milostov@b-zion.org.il>
2020-05-31 11:41:22 +0300

    End section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,14 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy",policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+
             services.AddControllers();
         }
 
@@ -43,6 +51,8 @@ namespace API
 
             //app.UseHttpsRedirection();
 
+             app.UseCors("CorsPolicy");
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -51,6 +61,8 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+
+           
         }
     }
 }
